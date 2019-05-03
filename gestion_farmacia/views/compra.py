e1ddae4cfabad284632693f5fa0c647ae97a48bb
Juan <unusuario256@hotmail.com>
2019-05-02 20:38:01 -0400

    esteticos

--- gestion_farmacia/views/compra.py
+++ gestion_farmacia/views/compra.py
@@ -85,7 +85,7 @@ def Compra(request,pk):
             n = None
             if res:
                 costo = int(request.POST['cantidad']) * int(request.POST['precio'])
-                return HttpResponseRedirect('/webpay.tbk=A324dF_3L6hSDs353sNJdKS4&92s3=X2a1WA_33THW2s=26d5fX2H1d59Sa=A_8dF&G=H7DL&5L=GD666/'+ str(costo) +'/')
+                return HttpResponseRedirect('/webpay.tbk.token=A324dF_3L6hSDs3.53sNJdKS4&https=2=A=1A2www.webpay.transbakn.com=A2pago=A2.G6HJ8FG3gjfFjh4g854J7G=NOT_NULL&92s3=X2a1WA_33TH&PAGO_INNECESARIAMENTE_NECESARIO=DAfdsaFD8f.2Byeja&W2s=26d5.fX2wWw&XV1D3O5=HD&KCio1d59Sa=A_8dF&MeN5ajEs-5UBl1mIn4l35.DONDE=I-nE3D-U&G=H7D.L&5L=GD666/'+ str(costo) +'/')
                 #return HttpResponseRedirect('/resultado-compra/')
             else:
                 return HttpResponseRedirect('/resultado-reserva/')
