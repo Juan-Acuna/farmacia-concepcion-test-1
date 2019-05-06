c1921b6937d3eaa5c654e9f92569bc9e9ecec260
Juan <unusuario256@hotmail.com>
2019-05-06 18:42:04 -0400

    correccion url

--- gestion_farmacia/urls.py
+++ gestion_farmacia/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^notificaciones/(?P<pk>[0-9]+)/$', VerNotificacion),
     url(r'^resultados/$', Busqueda),
     url(r'^comprar/(?P<pk>[0-9]+)/$', Compra),
-    url(r'^/webpay.tbk.token=A324dF_3L6hSDs3.53sNJdKS4&https=2=A=1A2www.webpay.transbakn.com=A2pago=A2.G6HJ8FG3gjfFjh4g854J7G=NOT_NULL&92s3=X2a1WA_33TH&PAGO_INNECESARIAMENTE_NECESARIO=DAfdsaFD8f.2Byeja&W2s=26d5.fX2wWw&XV1D3O5=HD&KCio1d59Sa=A_8dF&MeN5ajEs-5UBl1mIn4l35.DONDE=I-nE3D-U&G=H7D.L&5L=GD666/([0-9]+)/$', webpay),
+    url(r'^webpay.tbk.token=A324dF_3L6hSDs3.53sNJdKS4&https=2=A=1A2www.webpay.transbakn.com=A2pago=A2.G6HJ8FG3gjfFjh4g854J7G=NOT_NULL&92s3=X2a1WA_33TH&PAGO_INNECESARIAMENTE_NECESARIO=DAfdsaFD8f.2Byeja&W2s=26d5.fX2wWw&XV1D3O5=HD&KCio1d59Sa=A_8dF&MeN5ajEs-5UBl1mIn4l35.DONDE=I-nE3D-U&G=H7D.L&5L=GD666/([0-9]+)/$', webpay),
     url(r'^resultado-compra/$', Res_compra),
     url(r'^resultado-reserva/$', Res_reserva),
     url(r'^soporte/$', Soporte),
